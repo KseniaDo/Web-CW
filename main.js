93d0394e31c8d3d75c8177e740b149b477274a78
KseniaDo <54909463+KseniaDo@users.noreply.github.com>
2023-05-15 17:06:22 +0300

    final_result_pages

--- main.js
+++ main.js
@@ -2,29 +2,30 @@ let slider1 = document.querySelector('#sliderJs1')
 let slider2 = document.querySelector('#sliderJs2')
 let slider3 = document.querySelector('#sliderJs3')
 
-let offset1 = 0
-let offset2 = 0
-let offset3 = 0
-setInterval(function(){
-    offset1-=500
-    if(offset1 < -500*3){
-        offset1=0
+let elemWidth1 = 0
+let elemWidth2 = 0
+let offset = 0
+setInterval(function () {
+    elemWidth1 = document.querySelector('.slider_image').offsetWidth
+    if ((elemWidth1 != elemWidth2) && (elemWidth2 != 0)) {
+        offset = 0
+        slider1.style.left = offset + 'px'
+        slider2.style.left = offset + 'px'
+        slider3.style.left = offset + 'px'
     }
-    slider1.style.left = offset1 + 'px'
-}, 2000)
-
-setInterval(function(){
-    offset2-=300
-    if(offset2 < -300*3){
-        offset2=0
+    offset -= elemWidth1
+    if (offset < (-1) * elemWidth1 * 3) {
+        offset = 0
     }
-    slider2.style.left = offset2 + 'px'
-}, 2000)
-
-setInterval(function(){
-    offset3-=300
-    if(offset3 < -300*3){
-        offset3=0
+    elemWidth2 = document.querySelector('.slider_image').offsetWidth
+    if (elemWidth1 != elemWidth2) {
+        offset = 0
+        slider1.style.left = offset + 'px'
+        slider2.style.left = offset + 'px'
+        slider3.style.left = offset + 'px'
     }
-    slider3.style.left = offset3 + 'px'
+    slider1.style.left = offset + 'px'
+    slider2.style.left = offset + 'px'
+    slider3.style.left = offset + 'px'
+    elemWidth2 = document.querySelector('.slider_image').offsetWidth
 }, 2000)
